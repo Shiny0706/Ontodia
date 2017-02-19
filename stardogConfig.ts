9b827face30c3ae2491f392a126b89ba384ca0aa
Shiny0503 <superdev9676@gmail.com>
2017-02-19 13:48:41 +0300

    Move db connection information into configuration file

---
+++ stardogConfig.ts
@@ -0,0 +1,10 @@
+
+
+
+export default class Config {
+    public static readonly HOSTNAME = 'http://localhost';
+    public static readonly PORT = 5820;
+    public static readonly DB = 'csample';
+    public static readonly USERNAME = 'admin';
+    public static readonly PASSWORD = 'admin';
+}
